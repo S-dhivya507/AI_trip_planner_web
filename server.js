c360caa6b86be53d24f45eb6c670e45ae43786ab
s-dhivya507 <dhivyashanmugavel26@gmail.com>
2026-03-23 22:18:28 +0530

    Fix Express v5 wildcard route

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ async function callAI(prompt) {
 const distPath = path.join(__dirname, "dist");
 if (fs.existsSync(distPath)) {
   app.use(express.static(distPath));
-  app.get("/*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(distPath, "index.html"));
   });
 }
